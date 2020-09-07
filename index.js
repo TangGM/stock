0d7bbbb0512fcdf6abbb4f0d0f91c74b74c8fa65
Tang <tanghaha1018@gmail.com>
2020-09-07 19:15:33 +0800

    create procfile for heroku

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const request = require('request');
-
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000;
 
+// user body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 // API key pk_ff385012735d4dee8c75bc10bb484cd5
-function call_api(doneAPI) {
-    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_ff385012735d4dee8c75bc10bb484cd5', {json: true},
+function call_api(doneAPI, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_ff385012735d4dee8c75bc10bb484cd5', {json: true},
     (err,res, body) => {
         if (err) { return console.log(err); }
         console.log(body);
@@ -19,18 +23,27 @@ function call_api(doneAPI) {
     });
 };
 
-
 // set handlebars middlewares
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-// set handlebar routes
+// set handlebar get routes
 app.get('/', function (req, res) {
     call_api(function(doneAPI) {
             res.render('home', {
                 stock: doneAPI
             });
-    });
+    }, "tsm");
+});
+
+// set handlebar post routes
+app.post('/', function (req, res) {
+    call_api(function(doneAPI) {
+        // posted_stuff = req.body.stock_ticker
+        res.render('home', {
+            stock: doneAPI
+        });
+    }, req.body.stock_ticker);
 });
 
 // create about page route
